b3236c11c34475aafda629939830f952b4f8f0e4
vagrant <vagrant@vagrant.vm>
2017-05-13 00:27:24 +0000

    Working in server.py - homepage and login right now.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+""" Spot's server! """
 
+from jinja2 import StrictUndefined
 from flask import (Flask, render_template, redirect, request, flash, session,
                    jsonify)
 from flask_debugtoolbar import DebugToolbarExtension
@@ -11,7 +13,70 @@ from model import connect_to_db, db
 app = Flask(__name__)
 
 app.secret_key = "ABC"
-# app.jinja_env.undefined = StrictUndefined
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """ Homepage. """
+
+    sizes = Size.query.all()
+    groups = Group.query.all()
+    energies = Energy.query.all()
+    chars = Char.query.filter(Char.char_id > 12).all()
+
+    return render_template("homepage.html",
+                           groups=groups,
+                           sizes=sizes,
+                           energies=energies,
+                           chars=chars)
+
+
+@app.route('/login')
+def login():
+    """ Renders login page. """
+    return render_template('/login.html')
+
+
+@app.route('/login', methods=["POST"])
+def profile():
+    """ User's profile. """
+
+    email = request.form.get("email")
+    pwd = request.form.get("pwd")
+
+    if db.session.query(User).filter(User.email == email, User.password == pwd
+                                     ).first() is None:
+        flash("Email/password combination do not match.")
+        return redirect('/login')
+    else:
+        flash("Logged in! as %s" % email)
+        user = User.query.filter(User.email == email).one()
+        session['user_id'] = user.user_id
+        return redirect('/login')
+
+
+@app.route('/breed-search')
+def breed_search():
+    """ Breed search results. """
+    pass
+
+
+@app.route('/breed-search/<breed_id>')
+def breed_info(breed_id):
+    """ A breed's info page. """
+    pass
+
+
+@app.route('/breeder-search')
+def breeder_search():
+    """ Breeder search results. """
+    pass
+
+
+@app.route('/breeder-search/<breeder_id>')
+def breeder_info(breed_id):
+    pass
 
 
 if __name__ == "__main__":
