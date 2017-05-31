09f5197fd862787123bfad9a5f2bad522dc2d1f4
vagrant <vagrant@vagrant.vm>
2017-05-31 00:45:18 +0000

    Building real breeders' page

--- static/js/photo-gallery.js
+++ static/js/photo-gallery.js
@@ -1,15 +1,34 @@
 // JavaScript for photo galleries
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1;}
+//   if (n < 1) {slideIndex = x.length;}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
+
+var slideIndex = 1;
+var gallery = $(".photoGallery").data("gallery");
+showDivs(slideIndex, gallery);
+
+function plusDivs(n, gallery) {
+  showDivs(slideIndex += n, gallery);
 }
 
-function showDivs(n) {
+function showDivs(n, gallery) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("mySlides-"+gallery);
   if (n > x.length) {slideIndex = 1;}
   if (n < 1) {slideIndex = x.length;}
   for (i = 0; i < x.length; i++) {
